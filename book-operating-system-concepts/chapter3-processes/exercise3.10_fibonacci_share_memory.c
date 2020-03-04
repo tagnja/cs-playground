7c9df0c95a414446b38b3c37a567d2c6bd905d6d
taogen <taogenjia@gmail.com>
2020-03-04 16:14:57 +0800

    First commit

---
+++ book-operating-system-concepts/chapter3-processes/exercise3.10_fibonacci_share_memory.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+
+#define MAX_SEQUENCE 10
+
+typedef struct {
+	long fib_sequence[MAX_SEQUENCE];
+	int sequence_size;
+}shared_data;
+
+int main()
+{
+	int segment_id;
+	char* shared_memory;
+	
+	const int size = 4096;
+	
+	// allocate a shared memory segment
+	segment_id = shmget(IPC_PRIVATE, size, S_IRUSR | S_IWUSR);
+	
+	// attch 
+	shared_memory = (char *) shmat(segment_id, NULL, 0);
+	
+	// write a message to the shared memory segment
+	sprintf(shared_memory, "Hi There!");
+	
+	
+	printf("*%s\n", shared_memory);
+	
+	// detach the shared memory segment
+	shmdt(shared_memory);
+	
+	// reomve the shared memory segment
+	shmctl(segment_id, IPC_RMID, NULL);
+	
+	return 0;
+}
